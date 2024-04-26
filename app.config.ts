9bb01d876cb263ad0bc9b1262192600dc66cdb4b
Sean Velasco <seanvelasco@ieee.org>
2024-04-26 17:57:26 +0800

    Add Cloudflare Pages compatability

--- app.config.ts
+++ app.config.ts
@@ -1,3 +1,8 @@
 import { defineConfig } from "@solidjs/start/config"
 
-export default defineConfig({})
+export default defineConfig({
+    server: {
+        preset: "cloudflare-pages"
+    }
+
+})
