28057ab2dc50335640cbb42447b09a1be2c4696a
Sean Velasco <seanvelasco@ieee.org>
2024-07-01 23:19:02 +0800

    Remove unnecessary rollup config

--- app.config.ts
+++ app.config.ts
@@ -2,9 +2,6 @@ import { defineConfig } from "@solidjs/start/config"
 
 export default defineConfig({
     server: {
-        preset: "cloudflare-pages",
-        rollupConfig: {
-            external: ["__STATIC_CONTENT_MANIFEST", "node:async_hooks"]
-        }
+        preset: "cloudflare-pages"
     }
 })
