13e5363c2720ea14c734832cebb12a31bd7e808e
Sean Velasco <sean@sean.app>
2024-08-30 01:35:21 +0800

    Add arrow indicator if branch is opened/unopened, line to indicate hierarchy

--- src/components/Tree.tsx
+++ src/components/Tree.tsx
@@ -1,8 +1,8 @@
 import { For, Show, createSignal } from "solid-js"
 import { A, useLocation } from "@solidjs/router"
-// import Chevron from "./Chevron"
+import Chevron from "./Chevron"
 import styles from "./tree.module.css"
-import type { Leaf, Branch, Tree } from "../types"
+import type { Leaf, Branch, Tree } from "~/types"
 import { ALLOWED_FILES } from "~/lib/constants"
 
 const depth = (path: string) =>
@@ -12,7 +12,7 @@ const Leaf = (props: { leaf: Leaf }) => {
 	return (
 		<A
 			style={{
-				"padding-left": `${depth(props.leaf.path)}rem`
+				"padding-left": `calc(${depth(props.leaf.path)}rem + 7.5px + 1.25rem)`
 			}}
 			href={encodeURI(props.leaf.path)}
 			class={styles.leaf}
@@ -39,32 +39,36 @@ const Branch = (props: { branch: Branch }) => {
 		<>
 			<button
 				style={{
-					"padding-left": `${depth(props.branch.path)}rem`
+					"padding-left": `calc(${depth(props.branch.path)}rem + 7.5px)`
 				}}
 				class={styles.leaf}
 				onclick={toggle}
 			>
-				{/* <Chevron orientation={expanded() ? "bottom" : "right"} /> */}
+				 <Chevron orientation={expanded() ? "bottom" : "right"} />
 				<span class={styles.title}>{props.branch.title}</span>
 			</button>
 			<Show when={expanded() && props.branch.children}>
-				{(children) => <Branches tree={children()} />}
+				{(children) => <div class={styles.line} style={{
+					"--offset": `${depth(props.branch.path) + 1}rem`,
+				}}>
+					<Branches tree={children()} />
+				</div>}
 			</Show>
 		</>
 	)
 }
 
 const Branches = (props: { tree: Tree }) => (
-	<For each={props.tree}>
-		{(branch) => (
-			<Show
-				when={branch?.children?.length}
-				fallback={<Leaf leaf={branch} />}
-			>
-				<Branch branch={branch} />
-			</Show>
-		)}
-	</For>
+		<For each={props.tree}>
+			{(branch) => (
+				<Show
+					when={branch?.children?.length}
+					fallback={<Leaf leaf={branch} />}
+				>
+					<Branch branch={branch} />
+				</Show>
+			)}
+		</For>
 )
 
 const Tree = (props: { tree: Tree }) => (
