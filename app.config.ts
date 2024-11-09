24bc6e452c973a65c23c0c9f73b764a96732841e
Sean Velasco <sean@sean.app>
2024-11-09 17:34:38 +0800

    Use node

--- app.config.ts
+++ app.config.ts
@@ -2,7 +2,7 @@ import { defineConfig } from "@solidjs/start/config"
 
 export default defineConfig({
     server: {
-        preset: "vercel",
+        preset: "node",
         esbuild: {
             options: {
                 target: 'ESNext'
