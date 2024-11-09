e908d5dade7cde5df5772d9ee69d2f3093e32e69
Sean Velasco <sean@sean.app>
2024-11-09 17:25:10 +0800

    Try

--- app.config.ts
+++ app.config.ts
@@ -2,7 +2,7 @@ import { defineConfig } from "@solidjs/start/config"
 
 export default defineConfig({
     server: {
-        preset: "vercel-edge",
+        preset: "vercel",
         esbuild: {
             options: {
                 target: 'ESNext'
